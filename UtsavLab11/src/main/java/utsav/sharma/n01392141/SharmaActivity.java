74c5276a53d08625f74ae5fe136cf89c65c20b84
UtsavSharma2141 <n01392141@humbermail.ca>
2021-02-04 17:53:56 -0500

    Initial Commit

---
+++ UtsavLab11/src/main/java/utsav/sharma/n01392141/SharmaActivity.java
@@ -0,0 +1,17 @@
+/*
+Utsav Sharma n01392141 section C
+ */
+package utsav.sharma.n01392141;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SharmaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sharma);
+    }
+}
---
+++ UtsavLab11/src/main/java/utsav/sharma/n01392141/UtsavActivity.java
@@ -0,0 +1,18 @@
+/*
+Utsav Sharma n01392141 section C
+ */
+package utsav.sharma.n01392141;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//This is main entry for the application
+public class UtsavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
