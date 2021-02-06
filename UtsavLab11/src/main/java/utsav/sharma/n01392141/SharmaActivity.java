63d6a6109b4c9c8595fe24273c49685d9283b32e
UtsavSharma2141 <n01392141@humbermail.ca>
2021-02-05 21:21:16 -0500

    Update both files strings.xml and fr\strings.xml again. I also updated activity_main.xml, activity_sharma.xml, SharmaActivity.java, UtsavActivity.java files as per Assignment 1 requirements. Alao added one png file for assignment requirement.

--- UtsavLab11/src/main/java/utsav/sharma/n01392141/SharmaActivity.java
+++ UtsavLab11/src/main/java/utsav/sharma/n01392141/SharmaActivity.java
@@ -5,13 +5,34 @@ package utsav.sharma.n01392141;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class SharmaActivity extends AppCompatActivity {
 
+    TextView tv;
+    String st;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sharma);
+        tv=findViewById(R.id.textView);
+        st=getIntent().getExtras().getString("Value");
+        tv.setText(st);
+
     }
-}
+        @Override
+        protected void onStart() {
+            super.onStart();
+            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                    getString(R.string.snackbar1), Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
+
+
+    }
+
--- UtsavLab11/src/main/java/utsav/sharma/n01392141/UtsavActivity.java
+++ UtsavLab11/src/main/java/utsav/sharma/n01392141/UtsavActivity.java
@@ -5,14 +5,62 @@ package utsav.sharma.n01392141;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-//This is main entry for the application
+import com.google.android.material.snackbar.Snackbar;
+
 public class UtsavActivity extends AppCompatActivity {
+    Button btn;
+    EditText et;
+    String st;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btnActivity);
+        et = findViewById(R.id.edittext);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(UtsavActivity.this, SharmaActivity.class);
+                st = et.getText().toString();
+                i.putExtra("Value", st);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+    //
+    //
+    public void callIntent(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.btnBrowser:
+                Toast toast = Toast.makeText(getApplicationContext(), R.string.toast1_label, Toast.LENGTH_LONG);
+                toast.show();
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://humber.ca/"));
+                startActivity(intent);
+                break;
+            case R.id.btnActivity:
+                Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                        getString(R.string.snackbar1), Snackbar.LENGTH_LONG);
+                snackbar.show();
+                intent = new Intent(this, SharmaActivity.class);
+                startActivity(intent);
+            default:
+                break;
+        }
     }
 }
